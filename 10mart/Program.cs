f836075db941f85d98fb8316444792d100514b06
fuad-ismayil <ismayilfuad004@gmail.com>
2026-03-10 23:56:20 +0400

    30th task

--- 10mart/Program.cs
+++ 10mart/Program.cs
@@ -20,97 +20,95 @@
 //Console.WriteLine($"{soz} - {soz.Length} herf");
 
 //28
-string[] sual1 = ["ALMA", "Bu bir meyvedir. 3 cehd sansin var!"];
-string[] sual2 = ["KITAB", "Bu tehsile aid bir esyadir. 3 cehd sansin var!"];
-string[] sual3 = ["MONITOR", "Bu komputerin esas hisselerinden biridir. 3 cehd sansin var!"];
-string correct = "Duz tapdiniz!!!";
-string notCorrect = "Yanlis!!! qalan cehd sansiniz: ";
-string lose = "Meqlub oldunuz, bextinizi bir daha sinayin!";
-bool isTrue = false;
-Random rand = new Random();
-while (true)
-{
-    int randomNumber = rand.Next(1, 4);
-    switch (randomNumber)
-    {
-        case 1:
-            Console.WriteLine(sual1[1]);
-            for (int i = 0; i < 3; i++)
-            {
-                if (Console.ReadLine().ToUpper() == sual1[0])
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(correct);
-                    Console.ResetColor();
-                    isTrue = true;
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(notCorrect + (2 - i));
-                    Console.ResetColor();
-                }
-            }
-            break;
-        case 2:
-            Console.WriteLine(sual2[1]);
-            for (int i = 0; i < 3; i++)
-            {
-                if (Console.ReadLine().ToUpper() == sual2[0])
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(correct);
-                    Console.ResetColor();
-                    isTrue = true;
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(notCorrect + (2 - i));
-                    Console.ResetColor();
-                }
-            }
-            break;
-        case 3:
-            Console.WriteLine(sual3[1]);
-            for (int i = 0; i < 3; i++)
-            {
-                if (Console.ReadLine().ToUpper() == sual3[0])
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(correct);
-                    Console.ResetColor();
-                    isTrue = true;
-                    break;
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(notCorrect + (2 - i));
-                    Console.ResetColor();
-                }
-            }
-            break;
-    }
-    if (!isTrue)
-    {
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(lose);
-        Console.ResetColor();
-    }
-    Console.WriteLine("Yeniden oynamaq isteyirsiz? y/n");
-    if (Console.ReadLine() != "y")
-    {
-        Console.WriteLine("Sag olun!");
-        Console.ForegroundColor = ConsoleColor.Black;
-        break;
-    }
-    Console.Clear();
-}
-
-
+//string[] sual1 = ["ALMA", "Bu bir meyvedir. 3 cehd sansin var!"];
+//string[] sual2 = ["KITAB", "Bu tehsile aid bir esyadir. 3 cehd sansin var!"];
+//string[] sual3 = ["MONITOR", "Bu komputerin esas hisselerinden biridir. 3 cehd sansin var!"];
+//string correct = "Duz tapdiniz!!!";
+//string notCorrect = "Yanlis!!! qalan cehd sansiniz: ";
+//string lose = "Meqlub oldunuz, bextinizi bir daha sinayin!";
+//bool isTrue = false;
+//Random rand = new Random();
+//while (true)
+//{
+//    int randomNumber = rand.Next(1, 4);
+//    switch (randomNumber)
+//    {
+//        case 1:
+//            Console.WriteLine(sual1[1]);
+//            for (int i = 0; i < 3; i++)
+//            {
+//                if (Console.ReadLine().ToUpper() == sual1[0])
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Green;
+//                    Console.WriteLine(correct);
+//                    Console.ResetColor();
+//                    isTrue = true;
+//                    break;
+//                }
+//                else
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Red;
+//                    Console.WriteLine(notCorrect + (2 - i));
+//                    Console.ResetColor();
+//                }
+//            }
+//            break;
+//        case 2:
+//            Console.WriteLine(sual2[1]);
+//            for (int i = 0; i < 3; i++)
+//            {
+//                if (Console.ReadLine().ToUpper() == sual2[0])
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Green;
+//                    Console.WriteLine(correct);
+//                    Console.ResetColor();
+//                    isTrue = true;
+//                    break;
+//                }
+//                else
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Red;
+//                    Console.WriteLine(notCorrect + (2 - i));
+//                    Console.ResetColor();
+//                }
+//            }
+//            break;
+//        case 3:
+//            Console.WriteLine(sual3[1]);
+//            for (int i = 0; i < 3; i++)
+//            {
+//                if (Console.ReadLine().ToUpper() == sual3[0])
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Green;
+//                    Console.WriteLine(correct);
+//                    Console.ResetColor();
+//                    isTrue = true;
+//                    break;
+//                }
+//                else
+//                {
+//                    Console.ForegroundColor = ConsoleColor.Red;
+//                    Console.WriteLine(notCorrect + (2 - i));
+//                    Console.ResetColor();
+//                }
+//            }
+//            break;
+//    }
+//    if (!isTrue)
+//    {
+//        Console.ForegroundColor = ConsoleColor.Yellow;
+//        Console.WriteLine(lose);
+//        Console.ResetColor();
+//    }
+//    Console.WriteLine("Yeniden oynamaq isteyirsiz? y/n");
+//    if (Console.ReadLine() != "y")
+//    {
+//        Console.WriteLine("Sag olun!");
+//        Console.ForegroundColor = ConsoleColor.Black;
+//        break;
+//    }
+//    Console.Clear();
+//}
 
 //29 
 //for (int i = 0; i < 100; i++)
@@ -125,3 +123,31 @@ while (true)
 //    }
 //}
 
+//30
+int[] numbers = [10, 12, 22, 10, 32, 22, 16, 32, 22, 10, 11, 23, 32, 10, 10, 23, 23, 15];
+int uniqueCount = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    int selectedNumber = numbers[i];
+    bool isDublicate = false;
+    for (int d = 0; d < i; d++)
+    {
+        if (selectedNumber == numbers[d])
+        {
+            isDublicate = true;
+            Console.WriteLine("dublikat var");
+            break;
+        }
+    }
+    if (isDublicate == false)
+    {
+        uniqueCount++;
+    }
+}
+Console.WriteLine("Fərqli ədədlərin sayı: " + uniqueCount);
+
+//31
+
+//32
+
+//33
--- 10mart/obj/Debug/net10.0/10mart.AssemblyInfo.cs
+++ 10mart/obj/Debug/net10.0/10mart.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("10mart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b8126e3299da8837b8e29284c6e9a2c566bfc6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a682e5966007f73a6444b7af0f6ae05843f75737")]
 [assembly: System.Reflection.AssemblyProductAttribute("10mart")]
 [assembly: System.Reflection.AssemblyTitleAttribute("10mart")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
